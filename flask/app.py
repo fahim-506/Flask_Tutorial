cfa74376fe9fb10ea05ac9b755a617c1525f0ea5
fahim-506 <fahimp506@gmail.com>
2025-10-31 15:26:25 +0530

    simple flask app skeleton

---
+++ flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+###WSGI Application
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this Flask world"
+
+if __name__=="__main__":
+    app.run(debug = True)
